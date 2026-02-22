cd4fe7064413f6a3b19769297257e98972ddcd53
nicolas <nicolasbernard19@gmail.com>
2026-02-22 13:28:41 +0000

    in server.js create working CRUD

--- server/server.js
+++ server/server.js
@@ -15,23 +15,71 @@ const db = new pg.Pool({
     connectionString: process.env.DB_CONN,
 });
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "You've reached Nicolas's W7 project server"});
-});
-
-app.get("/number", async (req, res) => {
-    // res.json({message: "You have asked for a number"});
-    try {
-        // if you forget the extra ()'s it will break.
-        const numbers = (await db.query(`select * from test
-            ORDER BY id`)).rows;
-        res.json(numbers);
-    } catch {
-        res.send(`There was an error`);
-    }
-});
-
 const PORT=8080
 app.listen(PORT, () => {
     console.log(`Server is available at: http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json(
+        { message: "You've reached Nicolas's W7 project server"});
+});
+
+// Time to CRUD
+    let id = 5
+    let value = 9000
+    let table = "test"
+//CREATE
+app.get("/create", async (req, res) => {
+    try {
+        const create = await db.query(`INSERT INTO test (number) VALUES (3)`)
+        // const test = (await db.query(`INSERT INTO posts (title, content) VALUES ('cats',"they're the best")`))
+        res.send('I have created rows')
+    } catch {
+        res.send('there was a problem inserting into posts')
+    }
+});
+
+// READ
+app.get("/read", async (req, res) => {
+    // res.json({message: "You have asked for a number"});
+    try {
+        // if you forget the extra ()'s it will break.
+        const numbers = await db.query(`select * from test
+            ORDER BY id`);
+        console.log("I'm getting back numbers for you!");
+        res.json(numbers.rows);
+    } catch {
+        res.send(`I didn't read it :)`);
+    }
+});
+
+// UPDATE
+app.get("/update", async (req, res) => {
+
+    try {
+        const update = await db.query(
+            `UPDATE test SET number = (${value}) WHERE id = (${id})`);
+        console.log(`I updated id=${id} to ${value}`)
+    } catch {
+        res.send(`I didn't update :)`)
+    }
+});
+
+// DELETE
+app.get("/delete", async (req, res) => {
+    try {
+        const read = await db.query(
+            `SELECT from test WHERE id=${id}`
+        )
+        res.json(read)
+        const deletion = await db.query(
+            // `UPDATE test SET number = (${value}) WHERE id = (${id})`
+            `DELETE from test WHERE id=${id}`
+        );
+        console.log(`I deleted id=${id}`)
+        
+    } catch {
+        res.send(`I didn't delete :)`)
+    }
 });
