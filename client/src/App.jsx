8a2460e0523dd8afab28af21ef80597ecc8411a6
nicolas <nicolasbernard19@gmail.com>
2026-02-22 12:21:57 +0000

    Create client pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router'
 import './App.css'
 import TestPage from './TestPage'
+import Header from './components/Header'
 
 function App() {
     return(
+        <>
+        <h1>Hello there!</h1>
+        <Header />
         <TestPage />
+        </>
     )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter, Routes, Route, Link} from "react-router";
+
+export default function Header() {
+
+    return (
+        <div>
+        <header>
+            <h1>Sauce Site</h1>
+            
+        </header>
+        </div>
+    )
+}
