5eab8f87a77af4c6b0e72211b8d2c44486f57df0
nicolas <nicolasbernard19@gmail.com>
2026-02-26 20:35:58 +0000

    remove unused files

--- client/src/pages/BayPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function BayPage() {
-    return (
-        <div>
-            <p>This is a page abotu bay windows</p>
-            <p>saying lots of insightful things for sure</p>
-            <p>I like them in the sims</p>
-        </div>
-    )
-}
--- client/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-    return (
-        <div>
-            <marquee style={{fontFamily: 'monospace', fontSize: '32px'}}>I couldn't find that page???</marquee>
-        </div>
-    )
-}
